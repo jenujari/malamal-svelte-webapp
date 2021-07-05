627f33657530b8810da411fad7b607b3ae7d0213
Jenish Jariwala <jenish.j@crestinfosystems.com>
2021-07-05 23:40:03 +0530

    Initialize

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Header from "./../components/layouts/header.svelte"
+  import Sidebar from "./../components/layouts/sidebar.svelte"
+  import "../app.postcss";
+</script>
+
+<div class="root__layout">
+  <Header />
+  <main>
+    <Sidebar />
+    <div class="content">
+      <slot>
+      
+      </slot>
+    </div>
+  </main>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<h1>Hello and welcome to my site!</h1>
+
+<style lang="postcss">
+  h1 {
+    @apply text-red-500 text-2xl;
+  }
+</style>
